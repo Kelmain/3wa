607e5ed6b440e7d15d8d396363c719885e4d21f3
armin3wa <neumann.armin79@gmail.com>
2017-03-02 17:17:04 +0100

    mis a jour

--- exo_dev/modul1/js/caroussel.js
+++ exo_dev/modul1/js/caroussel.js
@@ -1,208 +1,140 @@
 'use strict';
-/*
-    global installEventHandler
-    global getRandomInteger
-*/
-
-const TESPACE = 32;
-const TGAUCHE = 37;
-const TDROITE = 39;
-const THAUT = 38;
-const TBAS = 40;
-
-var rotate = 0;
-
-var slides = [
+var slides;
+const ESPACE = 32;
+const GAUCHE = 37;
+const DROITE = 39;
+const HAUT = 38;
+const BAS = 40;
+slides = [
     {
         image: 'images/1.jpg',
         legend: 'Street Art'
-    },
-    {
+    }
+    , {
         image: 'images/2.jpg',
         legend: 'Fast Lane'
-    },
-    {
+    }
+    , {
         image: 'images/3.jpg',
         legend: 'Colorful Building'
-    },
-    {
+    }
+    , {
         image: 'images/4.jpg',
         legend: 'Skyscrapers'
-    },
-    {
+    }
+    , {
         image: 'images/5.jpg',
         legend: 'City by night'
-    },
-    {
+    }
+    , {
         image: 'images/6.jpg',
         legend: 'Tour Eiffel la nuit'
     }
 ];
+var etat;
 
-
-var state;
-
-function rotateSlider(event) {
-
-    if (event.deltaY >= 0) {
-        rotate += 10;
-        this.style.transform = "rotate(" + rotate + "deg)";
-        this.style.filter = "hue-rotate(" + rotate + "deg)";
-    } else {
-        rotate -= 10;
-        this.style.transform = "rotate(" + rotate + "deg)";
-        this.style.filter = "hue-rotate(" + rotate + "deg)";
+function onSliderNext() {
+    etat.index++;
+    if (etat.index == slides.length) {
+        etat.index = 0;
     }
-    console.log(event);
-}
-
-
-function onSliderGoToNext() {
-
-    state.index++;
-
-
-    if (state.index == slides.length) {
-
-        state.index = 0;
-    }
-
-
     refreshSlider();
+    init();
 }
 
-function onSliderGoToPrevious() {
-
-    state.index--;
-
-
-    if (state.index < 0) {
-
-        state.index = slides.length - 1;
-    }
-
-
+function onSliderFirst() {
+    etat.index = 0;
     refreshSlider();
+    init();
 }
 
-function onSliderGoToRandom() {
+function onSliderPrevious() {
+    etat.index--;
+    if (etat.index < 0) {
+        etat.index = slides.length - 1;
+    }
+    refreshSlider();
+    init();
+}
+
+function onSliderLast() {
+    etat.index = 5;
+    refreshSlider();
+    init();
+}
+
+function onSliderRandom() {
     var index;
-
     do {
-
         index = getRandomInteger(0, slides.length - 1);
     }
-    while (index == state.index);
-
-
-    state.index = index;
-
-
+    while (index == etat.index);
+    etat.index = index;
     refreshSlider();
 }
 
-
 function onSliderKeyUp(event) {
-
-
     switch (event.keyCode) {
-    case TDROITE:
-
-        onSliderGoToNext();
+    case DROITE:
+        onSliderNext();
         break;
-
-    case TESPACE:
-
+    case ESPACE:
         onSliderToggle();
         break;
-
-    case TGAUCHE:
-
-        onSliderGoToPrevious();
+    case GAUCHE:
+        onSliderPrevious();
         break;
-    case TBAS:
-
-        onSliderGoToPrevious();
+    case BAS:
+        onSliderLast();
         break;
-    case THaut:
-
-        onSliderGoToNext();
+    case HAUT:
+        onSliderFirst();
         break;
     }
 }
 
 function onSliderToggle() {
     var icon;
-
-
     icon = document.querySelector('#slider-toggle i');
-
     icon.classList.toggle('fa-play');
     icon.classList.toggle('fa-pause');
-
-
-    if (state.timer == null) {
-
-        state.timer = window.setInterval(onSliderGoToNext, 2000);
-
-        this.title = 'Arreter le carrousel';
+    if (etat.timer == null) {
+        etat.timer = window.setInterval(onSliderNext, 2000);
+        this.title = 'Arrêter le carrousel';
     } else {
-
-        window.clearInterval(state.timer);
-
-
-        state.timer = null;
-
-
-        this.title = 'Demarrer le carrousel';
+        window.clearInterval(etat.timer);
+        etat.timer = null;
+        this.title = 'Démarrer le carrousel';
     }
 }
 
 function onToolbarToggle() {
     var icon;
-
-
     icon = document.querySelector('#toolbar-toggle i');
-
     icon.classList.toggle('fa-arrow-down');
     icon.classList.toggle('fa-arrow-right');
-
-
     document.querySelector('.toolbar ul').classList.toggle('hide');
 }
 
 function refreshSlider() {
     var sliderImage;
     var sliderLegend;
-
-
     sliderImage = document.querySelector('#slider img');
     sliderLegend = document.querySelector('#slider figcaption');
-
-
-    sliderImage.src = slides[state.index].image;
-    sliderLegend.textContent = slides[state.index].legend;
+    sliderImage.src = slides[etat.index].image;
+    sliderLegend.textContent = slides[etat.index].legend;
+    return sliderImage.src;
 }
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    state = {};
-    state.index = 0;
-    state.timer = null;
-
-
-
-    installEventHandler('#slider-random', 'click', onSliderGoToRandom);
-    installEventHandler('#slider-previous', 'click', onSliderGoToPrevious);
-    installEventHandler('#slider-next', 'click', onSliderGoToNext);
-    installEventHandler('#slider-toggle', 'click', onSliderToggle);
-    installEventHandler('#toolbar-toggle', 'click', onToolbarToggle);
-    installEventHandler('#slider', 'wheel', rotateSlider);
-
-    document.addEventListener('keyup', onSliderKeyUp);
-
-    refreshSlider();
-});
+etat = {};
+etat.index = 0;
+etat.timer = null;
+installEventHandler('#slider-random', 'click', onSliderRandom);
+installEventHandler('#slider-previous', 'click', onSliderPrevious);
+installEventHandler('#slider-next', 'click', onSliderNext);
+installEventHandler('#slider-first', 'click', onSliderFirst);
+installEventHandler('#slider-last', 'click', onSliderLast);
+installEventHandler('#slider-toggle', 'click', onSliderToggle);
+installEventHandler('#toolbar-toggle', 'click', onToolbarToggle);
+installEventHandler('#slider', 'wheel', rotateSlider);
+document.addEventListener('keyup', onSliderKeyUp);
+refreshSlider();
--- exo_dev/modul1/js/utilities.js
+++ exo_dev/modul1/js/utilities.js
@@ -1,9 +1,14 @@
 'use strict';
-
+var rotate = 0;
+/*Retourne un entier aléatoire entre le minimum et le maximum donné dans l'appel de la fonction */
+/** min = int // max= int **/
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+/* Regroupe la recherche de l'element du DOM ainsi que l'ajout de l'ecouteur d'evenement qui lui est destiné */
+/** selector= id/class d'un element du DOM
+    type = evenement à controler
+   eventHandler = fonction à exectuer **/
 function installEventHandler(selector, type, eventHandler) {
     var domObject;
 
@@ -14,7 +19,9 @@ function installEventHandler(selector, type, eventHandler) {
     domObject.addEventListener(type, eventHandler);
 }
 
-
+function showImage(source) {
+    document.write('<img src="' + source + '">');
+}
 
 function requestInteger(message, min, max) {
     var integer
@@ -33,6 +40,18 @@ function requestInteger(message, min, max) {
 
 
 
-function showImage(source) {
-    document.write('<img src="' + source + '">');
+
+/* Rotation de l'element et de sa couleur sur le cercle chromatique en fonction du scroll (sens des aiguilles d'une montre pour un scroll vers le bas et sens inverse pour un scroll vers le haut)*/
+/** event = evenement **/
+function rotateSlider(event) {
+    if (event.deltaY >= 0) {
+        rotate += 10;
+        this.style.transform = "rotate(" + rotate + "deg)";
+        this.style.filter = "hue-rotate(" + rotate + "deg)";
+    } else {
+        rotate -= 10;
+        this.style.transform = "rotate(" + rotate + "deg)";
+        this.style.filter = "hue-rotate(" + rotate + "deg)";
+    }
+    console.log(event);
 }
